3dc2780914971b11da448bfdcd57dc80c9e6e0fe
island&lz <2466494557@qq.com>
2021-09-18 12:05:56 +0800

    Initial commit

---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,97 @@
+import { _decorator, Component, Vec3, systemEvent, SystemEvent, EventMouse, Animation, v3, SkeletalAnimation } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("PlayerController")
+export class PlayerController extends Component {
+    // for fake tween
+    private _startJump: boolean = false;
+    private _jumpStep: number = 0;
+    private _curJumpTime: number = 0;
+    private _jumpTime: number = 0.1;
+    private _curJumpSpeed: number = 0;
+    private _curPos: Vec3 = v3();
+    private _deltaPos: Vec3 = v3(0, 0, 0);
+    private _targetPos: Vec3 = v3();
+    private _isMoving = false;
+    private _curMoveIndex = 0;
+
+    start () {
+        // Your initialization goes here.
+        //systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    setInputActive(active: boolean) {
+        if (active) {
+            systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            systemEvent.off(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
+    }
+
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        } else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
+    }
+
+    jumpByStep(step: number) {
+        if (this._isMoving) {
+            return;
+        }
+        
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, v3(this._jumpStep, 0, 0));
+
+        this._isMoving = true;
+
+        this.CocosAnim.getState('cocos_anim_jump').speed = 3.5; //跳跃动画时间比较长，这里加速播放
+        this.CocosAnim.play('cocos_anim_jump'); //播放跳跃动画
+        if (step === 1) {
+            this.BodyAnim.play('oneStep');
+         } else if (step === 2) {
+            this.BodyAnim.play('twoStep');
+         }
+
+        this._curMoveIndex += step;//记录自己跳了多少步
+    }
+
+    onOnceJumpEnd() {
+        this._isMoving = false;
+        this.CocosAnim.play('cocos_anim_idle');
+        this.node.emit('JumpEnd', this._curMoveIndex);
+    }
+
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+                this.onOnceJumpEnd();
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
+    }
+
+    @property({type: Animation})
+    public BodyAnim: Animation = null;
+
+    reset() {
+        this._curMoveIndex = 0;
+    }
+
+    @property({type: SkeletalAnimation})
+    public CocosAnim: SkeletalAnimation = null;
+}
